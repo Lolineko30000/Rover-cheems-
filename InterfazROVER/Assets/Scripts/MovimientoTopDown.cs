5a2b21e5308b84166ac71c655af70923a1df04b1
Miguel Yáñez <migue.apy123@gmail.com>
2022-07-29 19:50:32 -0500

    Nuevo modelo ROVER
    >> Modelo de ROVER actualizado
    >> Script de movimiento en proceso de creación (Es extrañamente más complicado de lo que me gustaría aceptar)

--- InterfazROVER/Assets/Scripts/MovimientoTopDown.cs
+++ InterfazROVER/Assets/Scripts/MovimientoTopDown.cs
@@ -7,6 +7,7 @@ public class MovimientoTopDown : MonoBehaviour
 {
     //Estos valores pueden ser cambiados en Unity a consideración
     [SerializeField] private float velocidadMovimiento; 
+    [SerializeField] private float velocidadRotacion;
     [SerializeField] private Vector2 direccion;
 
     private Rigidbody2D RoverDowneyJr; //Declaración de objeto (ROVER)
@@ -19,20 +20,52 @@ public class MovimientoTopDown : MonoBehaviour
     void Start() 
     {
         RoverDowneyJr = GetComponent<Rigidbody2D>();
-        velocidadMovimiento = 5;
     }
 
-    
     void Update()
     {
-        //direccion = vector(x,y)
-        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized; 
+        //Esta linea indica posicion, direccion = vector(x,y) ; 0 < x < 1 & 0 < y < 1
+        //Justo esto es lo que necesitamos para cambiar los sprites
+        float valX = Input.GetAxisRaw("Horizontal");
+        float valY = Input.GetAxisRaw("Vertical");
+
+        direccion = new Vector2(valX, valY); 
+        float magnitudEntrada = Mathf.Clamp01(direccion.magnitude);
+        direccion.Normalize();
+
+        OrientacionRover(valX, valY);
     }
 
-    private void FixedUpdate() //Ni idea de que hace FixedUpdate, seguire investigando despues
+    private void FixedUpdate() //Esta función se ejecuta 50 veces por segundo
     {
         //Esta parte del codigo es la que permite que el robot se mueva
         RoverDowneyJr.MovePosition(RoverDowneyJr.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
+    }
+
+    /*
+    private bool viendoArriba = true;
+    private bool viendoIzquierda;
+    private bool viendoDerecha;
+    private bool viendoAbajo;
+    //Debug.Log("a");
+    */
+
+    void OrientacionRover(float x, float y)
+    {
+        /*
+        if(x > 0) //Va a la derecha
+
+        else if(x < 0) //Va a la izquierda
+        */
+
+        //Declaracion de no se k
+        if(direccion != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, direccion);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, velocidadRotacion * Time.deltaTime);
+        }
+        
+
     }
     
 
