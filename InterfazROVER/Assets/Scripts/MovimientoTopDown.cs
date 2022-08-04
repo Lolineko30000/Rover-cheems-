b3396e151732e4764e7460cd3aba77a7bec1419b
Lolineko30000 <317292181@pcpuma.acatlan.unam.mx>
2022-08-04 18:48:06 -0500

    Laberinto completado y movimiento autonomo.

--- InterfazROVER/Assets/Scripts/MovimientoTopDown.cs
+++ InterfazROVER/Assets/Scripts/MovimientoTopDown.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Laberinto;
 
 public class MovimientoTopDown : MonoBehaviour
 {
     [SerializeField] private float velocidadMovimiento; 
     [SerializeField] private float velocidadRotacion;
+    public bool control;
+    public bool done;
+    public int index;
+    //Orden de los nodos ue ebe ir recorriendo
+    public List<int> camino;
+    //Cordenadas de los nodos
+    public List<Transform> nodos;
  
     private float inputHorizontal, inputVertical;
     private Rigidbody2D RoverDowneyJr;
@@ -13,6 +22,16 @@ public class MovimientoTopDown : MonoBehaviour
     void Start() 
     {
         RoverDowneyJr = GetComponent<Rigidbody2D>();
+        
+        //Lineas especiales para obtener el camino del script del grafo.
+        GameObject aux = GameObject.Find("Grafo");
+        Grafo grafo = aux.GetComponent <Grafo> ();
+        camino = grafo.camino;
+        nodos = grafo.nodos;
+
+        control = false;
+        done = false;
+        index = 0;
     }
 
     private void InputEntrada()
@@ -37,44 +56,51 @@ public class MovimientoTopDown : MonoBehaviour
         //    return;
 
         float rotacion = -inputHorizontal * velocidadRotacion;
-            transform.Rotate(Vector3.forward * rotacion);
+        transform.Rotate(Vector3.forward * rotacion);
+  
     }
 
     void Update()
     {
-        InputEntrada();
+        if(control)
+            InputEntrada();
+        else
+        {
+            if(index == 0)
+            {
+                transform.Rotate(Vector3.forward * -4f);
+            }
+            if(index != camino.Count)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, nodos[camino[index]].position, Time.deltaTime * 4f);
+                
+                Vector3 NodoSiguiente = nodos[camino[index]].position - transform.position;
+                float angulo =  Mathf.Atan2(NodoSiguiente.y,NodoSiguiente.x)*Mathf.Rad2Deg;
+                Quaternion q = Quaternion.AngleAxis(angulo-90f, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 4f);
+               // Debug.Log(Vector3.Distance(transform.position, nodos[camino[index]].position));
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        MoverRobot();
-        RotarRobot();
+        if(control)
+        {
+            MoverRobot();
+            RotarRobot();
+        }
+    }
+
+    void Distancia()
+    {
+        if(Vector3.Distance(transform.position, nodos[camino[index]].position) < 0.5 && index != camino.Count-1)
+            index++;
     }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
     
     /*
     void Update()
